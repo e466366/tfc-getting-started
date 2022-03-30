8b1f1c5d285804b6ac8d1c93afaa71d3655d0a34
e466366 <33069176+e466366@users.noreply.github.com>
2022-03-30 16:49:34 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,13 +1,16 @@
 # The block below configures Terraform to use the 'remote' backend with Terraform Cloud.
 # For more information, see https://www.terraform.io/docs/backends/types/remote.html
+
+
+
+
 terraform {
   backend "remote" {
+    hostname = "app.terraform.io"
     organization = "tjp-test"
-
     workspaces {
       name = "hashicat-azure"
     }
   }
-
-  required_version = ">= 0.13.0"
 }
+
